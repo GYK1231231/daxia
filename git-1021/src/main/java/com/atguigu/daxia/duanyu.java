a55c1664492d34ec0a02d2494944b5da891d72db
duanyu <2456308558@qq.com>
2020-03-29 18:43:58 +0800

    erdilaile

--- git-1021/src/main/java/com/atguigu/daxia/duanyu.java
+++ git-1021/src/main/java/com/atguigu/daxia/duanyu.java
@@ -2,6 +2,7 @@ package com.atguigu.daxia;
 
 public class duanyu {
     public static void main(String[] args) {
-        System.out.println("woshierge");
+        System.out.println("woshierge1");
+        System.out.println("woshidageqiaofeng");
     }
 }
